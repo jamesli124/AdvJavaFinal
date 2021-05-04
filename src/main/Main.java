fdaf6a0f926610ab0e5822dde66b7128c7af73b2
jamesli124 <jameswli2021@gmail.com>
2021-05-04 13:53:11 -0400

    Created stage and blank scene in Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,10 +3,17 @@ package main;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 
 public class Main extends Application {
     @Override
     public void start(Stage primary) {
-        
+        primary.setTitle("Unnamed Game");
+
+        BorderPane bp = new BorderPane();
+
+        primary.setScene(new Scene(bp, 500, 500));
+
+        primary.show();
     }
 }
