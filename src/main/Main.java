42eca25cc9d4bb6fc6c383b6355e96c4931dce5a
jamesli124 <jameswli2021@gmail.com>
2021-05-04 13:46:37 -0400

    Created Main Class

---
+++ src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primary) {
+        
+    }
+}
