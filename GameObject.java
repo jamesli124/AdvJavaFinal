f30b0e644c0ca5cd0efc8537f888d653d305396d
A-Nipp <65468669+A-Nipp@users.noreply.github.com>
2021-05-05 15:49:04 -0400

    Add files via upload

---
+++ GameObject.java
@@ -0,0 +1,17 @@
+public abstract class GameObject 
+{
+    /*
+    This is any object in the game. Anything that can be drawn in the game. By definition, it will be intersectable 
+    */
+    /*
+    Will draw the object on the game screen.
+    */
+    public void render()
+    {
+
+    }
+    public static void main(String[] args)
+    {
+        System.out.println("Running tests for GameObject!")
+    }
+}
