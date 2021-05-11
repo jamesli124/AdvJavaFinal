c520a32b565d9ed8fcd4675d9c51e86da701263a
jamesli124 <58196853+jamesli124@users.noreply.github.com>
2021-05-11 13:50:23 -0400

    Added style.css, minor changes to Main.java formatting

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,18 +1,15 @@
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
-import javafx.scene.control.Slider;
-import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
 import javafx.scene.layout.GridPane;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Label;
 import javafx.geometry.Pos;
-import javafx.scene.control.Button;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.Priority;
 import javafx.scene.input.MouseButton;
@@ -25,6 +22,9 @@ import javafx.scene.text.Font;
 import javafx.scene.input.MouseEvent;
 import javafx.event.EventHandler;
 
+/**
+ * Main class which displays different scenes
+ */
 public class Main extends Application
 {
     public Main()
@@ -40,79 +40,83 @@ public class Main extends Application
     @Override
     public void start(Stage primary)
     {
-      Rectangle2D screenDimensions = Screen.getPrimary().getVisualBounds();
-      double screenWidth = screenDimensions.getWidth();
-      double screenHeight = screenDimensions.getHeight();
-      Image splashImage = new Image("https://imgs.xkcd.com/comics/excel_lambda_2x.png"); //"https://utility0.ncssm.edu/~morrison/images/samGetsIt.jpeg"
-      ImageView iv1 = new ImageView();
-      iv1.setImage(splashImage);
-      if (iv1.getImage().getWidth() > screenWidth) {
-        iv1.setFitWidth(screenWidth);
-        iv1.setPreserveRatio(true);
-        iv1.setSmooth(true);
-        iv1.setCache(true);
-      }
 
-      BorderPane bpSplash = new BorderPane();
-      bpSplash.setCenter(iv1);
-      bpSplash.setStyle("-fx-background-color: black");
-      Scene splash = new Scene(bpSplash, screenWidth, screenHeight);
-      primary.setWidth(screenDimensions.getWidth());
-      primary.setHeight(screenDimensions.getHeight());
+        Rectangle2D screenDimensions = Screen.getPrimary().getVisualBounds();
+        double screenWidth = screenDimensions.getWidth();
+        double screenHeight = screenDimensions.getHeight();
+
+        // Creating Scene splash
+        Image splashImage = new Image("https://imgs.xkcd.com/comics/excel_lambda_2x.png"); //"https://utility0.ncssm.edu/~morrison/images/samGetsIt.jpeg"
+        ImageView iv1 = new ImageView();
+        iv1.setImage(splashImage);
+        if (iv1.getImage().getWidth() > screenWidth) {
+            iv1.setFitWidth(screenWidth);
+            iv1.setPreserveRatio(true);
+            iv1.setSmooth(true);
+            iv1.setCache(true);
+        }
+        BorderPane bpSplash = new BorderPane();
+        bpSplash.setCenter(iv1);
+        bpSplash.setStyle("-fx-background-color: black");
+        Scene splash = new Scene(bpSplash, screenWidth, screenHeight);
+        primary.setWidth(screenDimensions.getWidth());
+        primary.setHeight(screenDimensions.getHeight());
 
 
-      /**
-      try {
+        /*
+        try {
         Thread.sleep(7500);
-      }
+        }
       catch(InterruptedException ex)
-      {
+        {
         Thread.currentThread().interrupt();
-      }
-      */
+        }
+        */
 
-      BorderPane bpMain = new BorderPane();
-      bpMain.setPrefSize(screenWidth / 4, screenHeight / 4);
-      TextField address = new TextField();
-      address.setFont(new Font ("Comic Sans MS Regular", 24));
-      address.setEditable(false);
-      address.setPrefWidth(screenWidth * .6);
-      /**
-      address.setStyle("-fx-text-fill: white");
-      address.setStyle("-fx-opacity: 1");
-      address.setStyle("-fx-background-color: black");
-      */
-      address.setText("If you have any complaints, please mail them to 612 CONCORDIA CT CHAPEL HILL, NC 27514");
-      Button play = new Button("Play");
-      Button options = new Button("Options");
-      Button complaints = new Button("Complaints?");
-      Button back = new Button("<-- Back");
-      complaints.setOnAction(e -> {
-        bpMain.setCenter(address);
-        bpMain.setLeft(back);
-      });
-      Button quit = new Button("Quit");
-      quit.setOnAction(e -> Platform.exit() );
-      VBox menuItems = new VBox();
-      menuItems.getChildren().addAll(play, options, complaints, quit);
+        // Creating Scene menu
+        BorderPane bpMain = new BorderPane();
+        bpMain.setPrefSize(screenWidth / 4, screenHeight / 4);
+        Label address = new Label();
+        address.setFont(new Font ("Comic Sans MS", 24));
+        // address.setEditable(false);
+        address.setPrefWidth(screenWidth * .6);
+        /*
+        address.setStyle("-fx-text-fill: white");
+        address.setStyle("-fx-opacity: 1");
+        address.setStyle("-fx-background-color: black");
+        */
+        address.setText("If you have any complaints, please mail them to 612 CONCORDIA CT CHAPEL HILL, NC 27514");
+        Button play = new Button("Play");
+        Button options = new Button("Options");
+        Button complaints = new Button("Complaints?");
+        Button back = new Button("<-- Back");
+        complaints.setOnAction(e -> {
+            bpMain.setCenter(address);
+            bpMain.setLeft(back);
+        });
+        Button quit = new Button("Quit");
+        quit.setOnAction(e -> Platform.exit() );
+        VBox menuItems = new VBox();
+        menuItems.getChildren().addAll(play, options, complaints, quit);
 
-      back.setOnAction(e -> {
-        bpMain.setLeft(null);
+        back.setOnAction(e -> {
+            bpMain.setLeft(null);
+            bpMain.setCenter(menuItems);
+        });
+
         bpMain.setCenter(menuItems);
-      });
+        //bpMain.setStyle("-fx-background-color: black");
 
-      bpMain.setCenter(menuItems);
-      //bpMain.setStyle("-fx-background-color: black");
+        Scene mainMenu = new Scene(bpMain, screenWidth, screenHeight);
 
-      Scene mainMenu = new Scene(bpMain, screenWidth, screenHeight);
-      iv1.setOnMouseClicked(e -> {
-        primary.setScene(mainMenu);
-      });
-      primary.setScene(splash);
-      primary.setWidth(screenDimensions.getWidth());
-      primary.setHeight(screenDimensions.getHeight());
-      primary.sizeToScene();
-      primary.show();
+        iv1.setOnMouseClicked(e -> {
+            primary.setScene(mainMenu);
+        });
+        primary.setScene(splash);
+        primary.setWidth(screenDimensions.getWidth());
+        primary.setHeight(screenDimensions.getHeight());
+        primary.sizeToScene();
+        primary.show();
     }
 
     @Override
