19c5732a4b307d0a7812b31427138a08262da54c
jamesli124 <58196853+jamesli124@users.noreply.github.com>
2021-05-11 13:14:11 -0400

    Made Renderable a functional interface and wrote doc comments

--- src/main/Renderable.java
+++ src/main/Renderable.java
@@ -2,6 +2,15 @@ package main;
 
 import javafx.scene.canvas.GraphicsContext;
 
+/**
+ * Functional interface specifying renderables
+ */
+@FunctionalInterface
 public interface Renderable {
+    /**
+     * Draws Renderable onto the Canvas with GraphicsContext pen
+     *
+     * @param pen   GraphicsContext to draw with
+     */
     public void render(GraphicsContext pen);
 }
